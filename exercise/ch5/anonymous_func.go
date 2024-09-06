cc11da336a8d4a1a692a0edadd46f60cd1cdc921
Veni <veni222987@gmail.com>
2024-09-06 19:27:29 +0800

    update 20240906

--- exercise/ch5/anonymous_func.go
+++
@@ -1,9 +0,0 @@
-package ch5
-
-func Squares() func() int {
-	var x int
-	return func() int {
-		x++
-		return x * x
-	}
-}
---
+++ exp/websocket/server/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Name      string `json:"name"`
+	Msg       string `json:"msg"`
+	Timestamp int64  `json:"timestamp"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func websocketHandler(w http.ResponseWriter, r *http.Request) {
+	// 协议升级
+	// ? 协议升级为什么是在服务端做的?
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		resMsg := "收到:" + string(p)
+		log.Println(string(resMsg))
+		if err := conn.WriteMessage(messageType, []byte(resMsg)); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "只接受POST请求", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// 读取请求体
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "无法读取请求体", http.StatusBadRequest)
+		return
+	}
+
+	resMsg := "收到:" + string(body)
+	log.Println(string(resMsg))
+
+	// 返回响应
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resMsg)
+}
+
+func main() {
+	http.HandleFunc("/websocket", websocketHandler)
+	http.HandleFunc("/http", httpHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
